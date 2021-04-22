c2866f896f256d296b7c42e3914a608b502d572a
verwildertjrl <80076842+verwildertjrl@users.noreply.github.com>
2021-04-22 15:30:47 -0500

    Update examenordinario.js

--- examenordinario.js
+++ examenordinario.js
@@ -6,14 +6,11 @@ function fboton(){
      request.send();
      request.onload= function (){
         const $button = document.querySelector("#bot").addEventListener("click",function (){
-         let casos = request.response;
-         console.log(casos);
- 
-         const $div= document.querySelector("#c");
-         
-             $div.innerHTML += `<p class="casos">Confirmados SARS-CoV-2: ${casos.Tamaulipas.confirmed}</p>`;
-        });
+            let casos = request.response;
+            console.log(casos);
+        const $div= document.querySelector("#c");
+             $div.innerHTML += `<p class="casos">Confirmados SARS-CoV-2: ${casos.Tamaulipas.confirmed}</p>`;});
      }
  }
-
  fboton();
+ 
