071a56df84136e9fdffe4023fc38261a1146f687
verwildertjrl <80076842+verwildertjrl@users.noreply.github.com>
2021-04-22 15:23:50 -0500

    ordi post

---
+++ examenordinario.js
@@ -0,0 +1,19 @@
+function fboton(){
+    
+    let request = new XMLHttpRequest();
+     request.responseType="json";
+     request.open("GET","https://covid-api.mmediagroup.fr/v1//cases?country=Mexico");
+     request.send();
+     request.onload= function (){
+        const $button = document.querySelector("#bot").addEventListener("click",function (){
+         let casos = request.response;
+         console.log(casos);
+ 
+         const $div= document.querySelector("#c");
+         
+             $div.innerHTML += `<p class="casos">Confirmados SARS-CoV-2: ${casos.Tamaulipas.confirmed}</p>`;
+        });
+     }
+ }
+
+ fboton();
